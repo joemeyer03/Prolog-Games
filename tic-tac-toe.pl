0f3d5f94059bd6aba03c5b066535692addb71b3b
Joe Meyer <34402933+joemeyer03@users.noreply.github.com>
2023-11-24 21:38:18 -0500

    Update tic-tac-toe.pl
    Added difficulties and fixed a bug

--- tic-tac-toe.pl
+++ tic-tac-toe.pl
@@ -1,6 +1,6 @@
 % Improved code from Programming Language Pragmatics by Michael L. Scott
 % Plays a tic-tac-toe game where you are X and go first. 
-% Hard to beat but it is possible
+% Can play on easy, medium, hard or impossible
 
 ordered_line(1, 2, 3).
 ordered_line(4, 5, 6).
@@ -32,28 +32,30 @@ win(A) :- o(B), o(C), line(A, B, C).
 block_win(A) :- x(B), x(C), line(A, B, C).
 split(A) :- o(B), o(C), different(B, C), line(A, B, D), line(A, C, E), empty(D), empty(E).
 block_split(A) :- x(B), x(C), different(B, C), line(A, B, D), line(A, C, E), empty(D), empty(E).
+build(A) :- o(B), line(A, B, C), empty(C).
 strong_build(A) :- o(B), line(A, B, C), empty(C), not(risky(C)).
 weak_build(A) :- o(B), line(A, B, C), empty(C), not(double_risky(C)).
-risky(_) :- o(D), line(_, D, E), empty(E).
-double_risky(C) :- o(D), o(E), different(D, E), line(C, D, F), line(C, E, G), empty(F), empty(G).
+risky(C) :- x(D), line(C, D, E), empty(E).
+double_risky(C) :- x(D), x(E), different(D, E), line(C, D, F), line(C, E, G), empty(F), empty(G).
 
-good(A) :- win(A).
-good(A) :- block_win(A).
-good(A) :- split(A).
-good(A) :- block_split(A).
-good(A) :- strong_build(A).
-good(A) :- weak_build(A).
-good(5).
-good(1).
-good(3).
-good(7).
-good(9).
-good(4).
-good(2).
-good(6).
-good(8).
+good(A, _) :- win(A).
+good(A, _) :- block_win(A).
+good(A, D) :- different(D, e), split(A).				% don't do splits on easy mode
+good(A, D) :- same(D, h), block_split(A).				% don't block splits on easy, medium, or impossible (better version for impossible below)
+good(A, D) :- different(D, i), build(A).				% build on all but impossible
+good(A, D) :- same(D, i), strong_build(A).				% do strong and weak build just for impossible
+good(A, D) :- same(D, i), weak_build(A).
+good(5, _).
+good(1, _).
+good(3, _).
+good(7, _).
+good(9, _).
+good(4, _).
+good(2, _).
+good(6, _).
+good(8, _).
 
-move(A) :- good(A), empty(A), !.
+move(A, D) :- good(A, D), empty(A), !.
 
 board_full :- full(1), full(2), full(3), full(4), full(5), full(6), full(7), full(8), full(9).
 
@@ -61,8 +63,8 @@ done :- ordered_line(A, B, C), o(A), o(B), o(C), write('I WIN!!!'), nl.
 done :- board_full, write('Draw. Try again'), nl.
 
 getmove :- write('Please enter a move: '), read(X), empty(X), assert(x(X)).
-makemove :- move(X), !, assert(o(X)).
-makemove :- board_full.
+makemove(D) :- move(X, D), !, assert(o(X)).
+makemove(_) :- board_full.
 
 printsquare(N) :- o(N), write(' o ').
 printsquare(N) :- x(N), write(' x ').
@@ -76,6 +78,6 @@ clear :- x(A), retract(x(A)), fail.
 clear :- o(A), retract(o(A)), fail.
 
 % main target, to run consult this file then query play
-play :- not(clear), repeat, getmove, respond.
-respond :- ordered_line(A, B, C), x(A), x(B), x(C), printboard, write('You won.'), nl. 
-respond :- makemove, printboard, done.
+play(D) :- not(clear), repeat, getmove, respond(D).
+respond(_) :- ordered_line(A, B, C), x(A), x(B), x(C), printboard, write('You won.'), nl. 
+respond(D) :- makemove(D), printboard, done.
